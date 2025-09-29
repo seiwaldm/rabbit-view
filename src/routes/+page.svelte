cb709daecf10425614ed536e93099f8cdf706e9e
seiwaldm <seiwald.markus@johak.at>
2025-09-29 11:14:55 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	let rabbits = $state([]);
+
+	async function listRabbits() {
+		const response = await fetch('http://localhost:7070/rabbits');
+		console.log('response: ', response);
+	}
+
+	$effect(() => listRabbits());
+</script>
+
+<h1>Our Rabbits</h1>
